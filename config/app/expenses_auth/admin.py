307b448fc8495d04c91688b3c8db9b30e94f3239
VISHALYADAV2622 <vishal@bancapp.in>
2026-01-10 22:20:35 +0530

    add user

--- config/app/expenses_auth/admin.py
+++ config/app/expenses_auth/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth import get_user_model
 
-# Register your models here.
+User = get_user_model()
+
+
+@admin.register(User)
+class CustomUserAdmin(UserAdmin):
+    model = User
+    list_display = (
+        "username",
+        "email",
+        "mobile_number",
+        "is_staff",
+        "is_active",
+    )
+    search_fields = ("username", "email", "mobile_number")
+
+    fieldsets = UserAdmin.fieldsets + (
+        ("Additional Info", {
+            "fields": ("date_of_birth", "mobile_number", "address"),
+        }),
+    )
